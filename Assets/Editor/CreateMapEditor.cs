1afc61927dc103ff11c25332a121a64f47ed21b7
stopresent <86364202+stopresent@users.noreply.github.com>
2024-05-15 01:49:16 +0900

    [ADD] 폰트랑 맵 파싱

--- Assets/Editor/CreateMapEditor.cs
+++ Assets/Editor/CreateMapEditor.cs
@@ -15,6 +15,85 @@ public class CreateMapEditor : MonoBehaviour
     [MenuItem("Tools/CreateMap %#c")]
     private static void CreateMap()
     {
+        #region ExcelData
+        string[] lines = File.ReadAllText($"{Application.dataPath}/@Resources/Data/Excel/MapData.csv").Split("\n");
+
+        GameObject parent = GameObject.Find("Parent");
+        float coX = 0, coY = 0, coZ = 0;
+        float toAdd = 1f;
+        float addToFloorY = 0.465f;
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            string[] row = lines[y].Replace("\r", "").Split(',');
+
+            if (row.Length == 0)
+                continue;
+            if (string.IsNullOrEmpty(row[0]))
+                continue;
+
+            for (int x = 0; x < row.Length; ++x)
+            {
+                string block = row[x];
+
+                if (block == "0")
+                {
+                    GameObject voidObject = Resources.Load<GameObject>($"Tilemap_0");
+                    UnityEngine.Object.Instantiate(voidObject, new Vector3(coX, coY + addToFloorY, coZ), Quaternion.identity, parent.transform);
+                }
+                else if (block[0] == 'I') // 아이템일 경우
+                {
+                    GameObject floor = Resources.Load<GameObject>($"Tilemap_1");
+                    UnityEngine.Object.Instantiate(floor, new Vector3(coX, coY + addToFloorY, coZ), Quaternion.identity, parent.transform);
+                    floor.transform.localScale = new Vector3(0.312f, 0.312f, 0.312f);
+
+                    // TODO 아이템 생성
+
+                }
+                else if (block[0] == 'M') // 몬스터일 경우
+                {
+                    GameObject floor = Resources.Load<GameObject>($"Tilemap_1");
+                    UnityEngine.Object.Instantiate(floor, new Vector3(coX, coY + addToFloorY, coZ), Quaternion.identity, parent.transform);
+                    floor.transform.localScale = new Vector3(0.312f, 0.312f, 0.312f);
+
+                    // TODO 몬스터 생성
+                    GameObject monster = Resources.Load<GameObject>($"Monster");
+                    UnityEngine.Object.Instantiate(monster, new Vector3(coX, coY + 1f, coZ), Quaternion.identity, parent.transform);
+                    monster.transform.localScale = new Vector3(1f, 1.4f, 1.4f);
+                    monster.GetComponent<MonsterController>().id = block[2] - '0';
+                }
+                else if (block[0] == 'B') // 보스 몬스터일 경우
+                {
+                    GameObject floor = Resources.Load<GameObject>($"Tilemap_1");
+                    UnityEngine.Object.Instantiate(floor, new Vector3(coX, coY + addToFloorY, coZ), Quaternion.identity, parent.transform);
+                    floor.transform.localScale = new Vector3(0.312f, 0.312f, 0.312f);
+
+                    // TODO 보스 몬스터 생성
+                }
+                else
+                {
+                    GameObject floor = Resources.Load<GameObject>($"Tilemap_1");
+                    UnityEngine.Object.Instantiate(floor, new Vector3(coX, coY + addToFloorY, coZ), Quaternion.identity, parent.transform);
+                    floor.transform.localScale = new Vector3(0.312f, 0.312f, 0.312f);
+                    // TODO 타일 생성
+                    if (block != "1")
+                    {
+                        GameObject tile = Resources.Load<GameObject>($"Tilemap_{block}");
+                        UnityEngine.Object.Instantiate(tile, new Vector3(coX, coY, coZ), Quaternion.identity, parent.transform);
+                        tile.transform.localScale = new Vector3(0.312f, 0.312f, 0.312f);
+                    }
+                }
+                coX += toAdd;
+            }
+            coZ += toAdd;
+            coX = 0;
+        }
+
+        #endregion
+    }
+
+    private static void CreateMap_Test()
+    {
         GameObject parent = GameObject.Find("Parent");
         if (parent == null)
             Object.Instantiate(parent);
