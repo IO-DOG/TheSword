687779764354b3b3f7ca92e6d6c90d4f6dd06e85
truemanburbank <71928366+truemanburbank@users.noreply.github.com>
2024-05-15 16:21:15 +0900

    [Edit] Add Player Movement InterpolatePos

--- Assets/@Scripts/Controllers/PlayerController.cs
+++ Assets/@Scripts/Controllers/PlayerController.cs
@@ -11,23 +11,20 @@ public class PlayerController : MonoBehaviour
     float _duration;
     bool _isMoving = false;
 
-    Vector3 _interpolatePos = new Vector3(0.5f, 0, 0.5f);
-    Vector3Int _cellPos = Vector3Int.zero;
+    Vector3 _interpolatePos = new Vector3(0.5f, 1.5f, Mathf.Sqrt(2)/2);
+    Vector3 _cellPos;
 
     void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
 
-        GameObject startPoint = GameObject.Find("StartPoint");
-        Vector3 pos = _grid.CellToWorld(_cellPos);
         _duration = 1 / _speed;
-        transform.position = pos;
-        if (startPoint != null)
-        {
-            transform.position = startPoint.transform.position;
-            _cellPos = new Vector3Int((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
-        }
+
+        Vector3 startPoint = GameObject.Find("StartPoint").transform.position + _interpolatePos;
+        transform.position = startPoint;
+        _cellPos = startPoint;
+
         Managers.Game.Player = this; // 던전 돌 때 죽으면 연동하도록 하기 위함.
     }
 
@@ -78,7 +75,7 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        transform.DOMove(_cellPos + _interpolatePos, _duration).OnComplete(()=> _isMoving = false);
+        transform.DOMove(_cellPos, _duration).OnComplete(()=> _isMoving = false);
     }
     #endregion
 }
