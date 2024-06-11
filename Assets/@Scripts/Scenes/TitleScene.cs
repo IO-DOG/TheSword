fff2bb032fe62f744294fbfd4dacffada8f5789e
Stopresent <86364202+stopresent@users.noreply.github.com>
2024-06-12 00:08:31 +0900

    [TEST] for Test

--- Assets/@Scripts/Scenes/TitleScene.cs
+++ Assets/@Scripts/Scenes/TitleScene.cs
@@ -9,6 +9,13 @@ public class TitleScene : BaseScene
         base.Init();
 
         SceneType = Define.Scene.TitleScene;
+
+        // 현재야 생각을 해볼까?
+        // 혹시 알고있는지 모르겠지만 넌 사람이란다.
+
+        GameObject ui_titleScene = GameObject.Find("UI_TitleScene");
+
+
         //TitleUI
     }
 
