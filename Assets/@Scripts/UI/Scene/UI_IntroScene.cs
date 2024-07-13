e678f59d67f063bcab569d0fe1689ce450ce96a7
stopresent <86364202+stopresent@users.noreply.github.com>
2024-07-13 20:32:13 +0900

    [UPDATE] 인트로 씬 사진 변경

--- Assets/@Scripts/UI/Scene/UI_IntroScene.cs
+++ Assets/@Scripts/UI/Scene/UI_IntroScene.cs
@@ -65,23 +65,44 @@ public class UI_IntroScene : UI_Scene
             Managers.Resource.Load<Sprite>("Intro02"),
             Managers.Resource.Load<Sprite>("Intro03"),
             Managers.Resource.Load<Sprite>("Intro04"),
-            Managers.Resource.Load<Sprite>("Intro01"),
+            Managers.Resource.Load<Sprite>("Intro05"),
         };
+
+        if (idx == totalCount)
+        {
+            Managers.Scene.LoadScene(Define.Scene.TutorialScene);
+            return;
+        }
+
+        GetImage((int)Images.SceneImage).sprite = ImageList[idx - 1];
+        GetText((int)Texts.SceneText).text = Managers.GetString(_scripts[idx].id);
 
         if (idx == 1) // 처음 클릭시
         {
             GetImage((int)Images.SceneImage).gameObject.SetActive(true);
             GetText((int)Texts.SceneText).gameObject.transform.position = Util.WorldToScreenCood(new Vector3(0, -400, 0));
         }
-        if (idx == totalCount)
+        if (idx == totalCount - 2)
         {
-            Managers.Scene.LoadScene(Define.Scene.TutorialScene);
-            return;
+            StartCoroutine(CoInvertedImage());
         }
-        GetImage((int)Images.SceneImage).sprite = ImageList[idx - 1];
-        GetText((int)Texts.SceneText).text = Managers.GetString(_scripts[idx].id);
-
+        if (idx == totalCount - 1)
+        {
+            GetImage((int)Images.SceneImage).sprite = ImageList[idx - 1];
+            GetImage((int)Images.SceneImage).SetNativeSize();
+            GetImage((int)Images.SceneImage).transform.position = new Vector3(960, 540, 0);
+            GetImage((int)Images.SceneImage).transform.localScale = new Vector3(0.34f, 0.34f, 0);
+        }
+        
         idx++;
         idx = Mathf.Min(idx, totalCount);
+    }
+
+    IEnumerator CoInvertedImage()
+    {
+        WaitForSeconds delay = new WaitForSeconds(1f);
+        GetImage((int)Images.SceneImage).sprite = Managers.Resource.Load<Sprite>("Intro03");
+        yield return delay;
+        GetImage((int)Images.SceneImage).sprite = Managers.Resource.Load<Sprite>("Intro04");
     }
 }
