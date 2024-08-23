0849947eb8fc8c7d12cd271ecc510f4eb9049379
truemanburbank <juxtap.se@gmail.com>
2024-08-23 16:40:38 +0900

    [FIX] 어드레서블, 테스트 코드 수정

--- Assets/@Scripts/Scenes/TutorialScene.cs
+++ Assets/@Scripts/Scenes/TutorialScene.cs
@@ -54,10 +54,10 @@ public class TutorialScene : BaseScene
 
         yield return new WaitForSeconds(Define.STAGE_NAME_DURATION);
 
-        Managers.UI.ShowPopupUI<UI_BossRoomCheckPopup>();
+        // Managers.UI.ShowPopupUI<UI_BossRoomCheckPopup>();
 
-        //UI_ConversationPopup conversation = Managers.UI.ShowPopupUI<UI_ConversationPopup>();
-        //conversation._eventID = Define.EVENT_TUTORIAL;
+        UI_ConversationPopup conversation = Managers.UI.ShowPopupUI<UI_ConversationPopup>();
+        conversation._eventID = Define.EVENT_TUTORIAL;
 
         // Reset Player Stat
         Managers.Game.Player.Speed = originalSpeed;
